cf9abeb633125259ec79bb760af190098df086be
ishaansehgal99 <ishaanforthewin@gmail.com>
2024-12-29 03:40:00 -0800

    Initial Commit

---
+++ HappinessJournal/HappinessJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappinessJournalApp.swift
+//  HappinessJournal
+//
+//  Created by Ishaan Sehgal on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct HappinessJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HappinessJournalTests/HappinessJournalTests.swift
@@ -0,0 +1,16 @@
+//
+//  HappinessJournalTests.swift
+//  HappinessJournalTests
+//
+//  Created by Ishaan Sehgal on 12/29/24.
+//
+
+import Testing
+
+struct HappinessJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
