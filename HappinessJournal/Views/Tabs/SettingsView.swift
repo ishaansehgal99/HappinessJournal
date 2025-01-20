e37f3a71bb164fc29dc214f416d7f8e6169c4c41
ishaansehgal99 <ishaanforthewin@gmail.com>
2025-01-19 16:29:12 -0800

    feat: Update the settings features

--- HappinessJournal/Views/Tabs/SettingsView.swift
+++ HappinessJournal/Views/Tabs/SettingsView.swift
@@ -11,43 +11,34 @@ import UserNotifications
 struct SettingsView: View {
     @ObservedObject private var user = User.sharedUser
     @State private var isTimePickerVisible = false
-    @State private var selectedTime = Date()
-    @State private var showAboutView = false
-    @State private var showFeedbackView = false
-    
+    @State private var selectedTime: Date = Date() // Fallback default
+
     var body: some View {
         NavigationView {
             Form {
+                // Notifications Section
                 Section(header: Text("Notifications")) {
                     Toggle("Daily Reminder", isOn: $user.hasReminder)
-                        .onChange(of: user.hasReminder) { oldReminder, newReminder in
-                            if newReminder {
-                                requestNotificationAuthorization()
-                                isTimePickerVisible = true
-                            } else {
-                                UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
-                                isTimePickerVisible = false
-                            }
+                        .onChange(of: user.hasReminder) { _, newValue in
+                            handleReminderToggle(isOn: newValue)
                         }
 
-                    if user.hasReminder && isTimePickerVisible {
+                    if user.hasReminder {
                         DatePicker(
                             "Reminder Time",
                             selection: $selectedTime,
                             displayedComponents: .hourAndMinute
                         )
-                        .onChange(of: selectedTime) { oldTime, newTime in
-                            user.reminderTime = formatTime(newTime)
-                            scheduleNotification(at: newTime)
+                        .onChange(of: selectedTime) { _, newTime in
+                            updateReminderTime(to: newTime)
                         }
                         .datePickerStyle(WheelDatePickerStyle())
                     }
                 }
 
+                // Support Section
                 Section(header: Text("Support")) {
-                    Button("About") {
-                        showAboutView = true
-                    }
+                    NavigationLink("About", destination: AboutView())
                     Button("Send Feedback") {
                         sendEmail(to: "threegoodthingscontact@gmail.com")
                     }
@@ -56,6 +47,7 @@ struct SettingsView: View {
                     }
                 }
 
+                // Social Media Section
                 Section(header: Text("Social Media")) {
                     Button("Visit Facebook") {
                         openURL("https://www.facebook.com/Three-Good-Things-A-Happiness-Journal-for-iOS-1793191400995847/")
@@ -65,23 +57,47 @@ struct SettingsView: View {
                     }
                 }
             }
-
             .navigationTitle("Settings")
-            .sheet(isPresented: $showAboutView) {
-                AboutView()
+            .onAppear {
+                initializeSelectedTime()
             }
         }
     }
 
     // MARK: - Helper Functions
 
-    private func requestNotificationAuthorization() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-            if !granted {
-                DispatchQueue.main.async {
-                    user.hasReminder = false
+    private func initializeSelectedTime() {
+        if let reminderTime = DateFormatter.reminderTimeFormatter.date(from: user.reminderTime) {
+            selectedTime = reminderTime
+        }
+    }
+
+    private func handleReminderToggle(isOn: Bool) {
+        if isOn {
+            requestNotificationAuthorization { granted in
+                if granted {
+                    isTimePickerVisible = true
+                } else {
+                    DispatchQueue.main.async {
+                        user.hasReminder = false
+                    }
                 }
             }
+        } else {
+            UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+            isTimePickerVisible = false
+        }
+    }
+
+    private func updateReminderTime(to time: Date) {
+        let formattedTime = DateFormatter.reminderTimeFormatter.string(from: time)
+        user.reminderTime = formattedTime
+        scheduleNotification(at: time)
+    }
+
+    private func requestNotificationAuthorization(completion: @escaping (Bool) -> Void) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, _ in
+            completion(granted)
         }
     }
 
@@ -101,28 +117,29 @@ struct SettingsView: View {
         }
     }
 
-    private func formatTime(_ date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "h:mm a"
-        return formatter.string(from: date)
-    }
-
     private func sendEmail(to email: String) {
         if let emailURL = URL(string: "mailto:\(email)"), UIApplication.shared.canOpenURL(emailURL) {
             UIApplication.shared.open(emailURL)
+        } else {
+            print("Unable to open email URL.")
         }
     }
 
     private func rateApp() {
         let appID = "1242079576" // Replace with your App Store ID
-        if let url = URL(string: "itms-apps://itunes.apple.com/app/viewContentsUserReviews?id=\(appID)") {
+        if let url = URL(string: "itms-apps://itunes.apple.com/app/viewContentsUserReviews?id=\(appID)"),
+           UIApplication.shared.canOpenURL(url) {
             UIApplication.shared.open(url)
+        } else {
+            print("Unable to open App Store URL.")
         }
     }
 
     private func openURL(_ urlString: String) {
         if let url = URL(string: urlString), UIApplication.shared.canOpenURL(url) {
             UIApplication.shared.open(url)
+        } else {
+            print("Unable to open URL: \(urlString)")
         }
     }
 }
@@ -130,16 +147,21 @@ struct SettingsView: View {
 struct AboutView: View {
     var body: some View {
         VStack {
-            Text("About")
-                .font(.largeTitle)
-                .padding()
-            Spacer()
             Text("Three Good Things is a Happiness Journal designed to help you reflect on the positives in life.")
                 .font(.body)
                 .padding()
             Spacer()
         }
+        .navigationTitle("About")
     }
+}
+
+extension DateFormatter {
+    static let reminderTimeFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "h:mm a"
+        return formatter
+    }()
 }
 
 #Preview {
